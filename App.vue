c1830bb718847b2e10011eea1ad835a2828c4dd5
kasha-wei <1529379104@qq.com>
2020-07-15 22:02:42 +0800

    my component

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from './components/MainTabBar'
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+  @import url('./assets/css/base.css');
+</style>
